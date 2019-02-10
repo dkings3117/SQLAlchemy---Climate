4586cfbe48599b44e1c4e290f72634cca864b564
Dave Kingsley <dkings3117@gmail.com>
2019-02-09 22:39:56 -0600

    updated

--- climate_app.py
+++ climate_app.py
@@ -27,16 +27,6 @@ Station = Base.classes.station
 # Create our session (link) from Python to the DB
 session = Session(engine)
 
-justice_league_members = [
-    {"superhero": "Aquaman", "real_name": "Arthur Curry"},
-    {"superhero": "Batman", "real_name": "Bruce Wayne"},
-    {"superhero": "Cyborg", "real_name": "Victor Stone"},
-    {"superhero": "Flash", "real_name": "Barry Allen"},
-    {"superhero": "Green Lantern", "real_name": "Hal Jordan"},
-    {"superhero": "Superman", "real_name": "Clark Kent/Kal-El"},
-    {"superhero": "Wonder Woman", "real_name": "Princess Diana"}
-]
-
 #################################################
 # Flask Setup
 #################################################
@@ -46,13 +36,6 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
-
-@app.route("/api/v1.0/justice-league")
-def justice_league():
-    """Return the justice league data as json"""
-
-    return jsonify(justice_league_members)
-
 
 @app.route("/")
 def welcome():
@@ -168,36 +151,6 @@ def tobs_by_start_end(start_date, end_date):
     # Create a dictionary from the row data and append to a list
     # return jsonify({"error": f"not found."}), 404
     return jsonify(all_start_end)
-
-@app.route("/api/v1.0/justice-league/real_name/<real_name>")
-def justice_league_by_real_name(real_name):
-    """Fetch the Justice League character whose real_name matches
-       the path variable supplied by the user, or a 404 if not."""
-
-    canonicalized = real_name.replace(" ", "").lower()
-    for character in justice_league_members:
-        search_term = character["real_name"].replace(" ", "").lower()
-
-        if search_term == canonicalized:
-            return jsonify(character)
-
-    return jsonify({"error": f"Character with real_name {real_name} not found."}), 404
-
-
-@app.route("/api/v1.0/justice-league/superhero/<superhero>")
-def justice_league_by_superhero__name(superhero):
-    """Fetch the Justice League character whose superhero matches
-       the path variable supplied by the user, or a 404 if not."""
-
-    canonicalized = superhero.replace(" ", "").lower()
-    for character in justice_league_members:
-        search_term = character["superhero"].replace(" ", "").lower()
-
-        if search_term == canonicalized:
-            return jsonify(character)
-
-    return jsonify({"error": "Character not found."}), 404
-
 
 if __name__ == "__main__":
     app.run(debug=True)
